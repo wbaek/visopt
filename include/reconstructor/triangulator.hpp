132ee61e2025fcae4074f81be718a75427c8300d
clint.baek <clint.baek@kakaocorp.com>
2017-01-27 00:10:11 +0900

    add reconstruction

---
+++ include/reconstructor/triangulator.hpp
@@ -0,0 +1,24 @@
+#ifndef __RECONSTRUCTOR_TRIANGULATOR_HPP__
+#define __RECONSTRUCTOR_TRIANGULATOR_HPP__
+
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include "reconstructor/reconstructor.hpp"
+
+namespace visopt{
+class Triangulator : public Reconstructor {
+    public:
+        Triangulator(const cv::Mat& intrinsic) {
+            this->intrinsic = intrinsic;
+        }
+        virtual ~Triangulator() {
+        }
+
+        virtual const std::vector<cv::Point3f> calc(const std::vector<KeyFrame>& keyframes) const;
+        
+    protected:
+        cv::Mat intrinsic;
+};
+}
+
+#endif //__RECONSTRUCTOR_TRIANGULATOR_HPP__
